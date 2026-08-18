3b291f4ab975b0779ecda4a4647a670bb465ba7e
kpstreem66-sys <kpstreem66@gmail.com>
2026-08-18 15:13:38 -0600

    refactor: move game source code to folder3
    Organize project structure by relocating game components and types to the `folder3` directory for better maintainability.

---
+++ src/folder3/index.ts
@@ -0,0 +1,7 @@
+export { default as PySpellGame } from './Game';
+export * from './types';
+export * from './data/platformLevels';
+export * from './data/spellRecipes';
+export * from './engine/physics';
+export * from './engine/pythonInterpreter';
+export * from './audio/soundFx';
